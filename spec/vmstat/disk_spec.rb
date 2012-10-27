ebe51aec75a4a3d6a053f6134959af03664fe8ad
Vincent Landgraf <setcool@gmx.de>
2012-10-27 17:36:47 +0200

    specs for the byte calculation

--- spec/vmstat/disk_spec.rb
+++ spec/vmstat/disk_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
 describe Vmstat::Disk do
+  context "sample" do
+    let(:disk) { described_class.new :hfs, "/dev/disk0", "/mnt/test",
+                                     4096, 100, 200, 600 }
+    subject { disk }
+
+    its(:type) { should == :hfs }
+    its(:origin) { should == "/dev/disk0" }
+    its(:mount) { should == "/mnt/test" }
+
+    its(:block_size) { should == 4096 }
+    its(:free_blocks) { should == 100 }
+    its(:available_blocks) { should == 200 }
+    its(:total_blocks) { should == 600 }
+
+    its(:free_bytes) { should == 409600 }
+    its(:available_bytes) { should == 819200 }
+    its(:used_bytes) { should == 2048000 }
+    its(:total_bytes) { should == 2457600 }
+  end
+
   context "Vmstat#disk" do
     let(:disk) { Vmstat.disk("/") }
     subject { disk }
 
     it "should be a vmstat disk object" do
-      should be_a(Vmstat::Disk)
+      should be_a(described_class)
     end
 
     context "methods" do
