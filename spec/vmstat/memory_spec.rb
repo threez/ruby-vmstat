c3894ff3433a77067f54170004f61c7647ed5e74
Daniel Berger <djberg96@gmail.com>
2020-05-20 05:20:19 -0400

    Applied rubocop -a to _memory_spec.rb.

--- spec/vmstat/memory_spec.rb
+++ spec/vmstat/memory_spec.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Vmstat::Memory do
-  context "sample" do
-    let(:disk) { described_class.new 4096, 775581, 196146, 437495, 685599,
-                                     1560532, 0 }
+  context 'sample' do
+    let(:disk) do
+      described_class.new 4096, 775_581, 196_146, 437_495, 685_599,
+                          1_560_532, 0
+    end
     subject { disk }
 
     describe '#pagesize' do
@@ -13,52 +17,52 @@ describe Vmstat::Memory do
 
     describe '#wired' do
       subject { super().wired }
-      it { is_expected.to eq(775581) }
+      it { is_expected.to eq(775_581) }
     end
 
     describe '#active' do
       subject { super().active }
-      it { is_expected.to eq(196146) }
+      it { is_expected.to eq(196_146) }
     end
 
     describe '#inactive' do
       subject { super().inactive }
-      it { is_expected.to eq(437495) }
+      it { is_expected.to eq(437_495) }
     end
 
     describe '#free' do
       subject { super().free }
-      it { is_expected.to eq(685599) }
+      it { is_expected.to eq(685_599) }
     end
 
     describe '#wired_bytes' do
       subject { super().wired_bytes }
-      it { is_expected.to eq(3176779776) }
+      it { is_expected.to eq(3_176_779_776) }
     end
 
     describe '#active_bytes' do
       subject { super().active_bytes }
-      it { is_expected.to eq(803414016) }
+      it { is_expected.to eq(803_414_016) }
     end
 
     describe '#inactive_bytes' do
       subject { super().inactive_bytes }
-      it { is_expected.to eq(1791979520) }
+      it { is_expected.to eq(1_791_979_520) }
     end
 
     describe '#free_bytes' do
       subject { super().free_bytes }
-      it { is_expected.to eq(2808213504) }
+      it { is_expected.to eq(2_808_213_504) }
     end
 
     describe '#total_bytes' do
       subject { super().total_bytes }
-      it { is_expected.to eq(8580386816) }
+      it { is_expected.to eq(8_580_386_816) }
     end
 
     describe '#pageins' do
       subject { super().pageins }
-      it { is_expected.to eq(1560532) }
+      it { is_expected.to eq(1_560_532) }
     end
 
     describe '#pageouts' do
@@ -67,15 +71,15 @@ describe Vmstat::Memory do
     end
   end
 
-  context "Vmstat#memory" do
+  context 'Vmstat#memory' do
     let(:memory) { Vmstat.memory }
     subject { memory }
 
-    it "should be a vmstat memory object" do
+    it 'should be a vmstat memory object' do
       is_expected.to be_a(described_class)
     end
 
-    context "methods" do
+    context 'methods' do
       it { is_expected.to respond_to(:pagesize) }
       it { is_expected.to respond_to(:wired) }
       it { is_expected.to respond_to(:active) }
@@ -88,10 +92,10 @@ describe Vmstat::Memory do
       it { is_expected.to respond_to(:active_bytes) }
       it { is_expected.to respond_to(:inactive_bytes) }
       it { is_expected.to respond_to(:free_bytes) }
-      it { is_expected.to respond_to(:total_bytes)}
+      it { is_expected.to respond_to(:total_bytes) }
     end
 
-    context "content" do
+    context 'content' do
       describe '#pagesize' do
         subject { super().pagesize }
         it { is_expected.to be_a_kind_of(Numeric) }
