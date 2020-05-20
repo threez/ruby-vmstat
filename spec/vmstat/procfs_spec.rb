7ef60f2b7d479492fbb8335c61721f11c445bade
Daniel Berger <djberg96@gmail.com>
2020-05-20 05:20:50 -0400

    Applied rubocop -a to procfs_spec.rb.

--- spec/vmstat/procfs_spec.rb
+++ spec/vmstat/procfs_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Vmstat::ProcFS do
@@ -6,66 +8,66 @@ describe Vmstat::ProcFS do
       extend Vmstat::ProcFS
 
       def self.procfs_path
-        File.expand_path("../../procfs", __FILE__)
+        File.expand_path('../procfs', __dir__)
       end
     end
   end
   subject { procfs }
 
-  context "#cpu" do
+  context '#cpu' do
     subject { procfs.cpu }
 
-    it { is_expected.to be_a(Array)}
-    it do 
+    it { is_expected.to be_a(Array) }
+    it do
       is_expected.to eq([
-        Vmstat::Cpu.new(0, 311, 966, 0, 26788),
-        Vmstat::Cpu.new(1, 351, 862, 0, 27263),
-        Vmstat::Cpu.new(2, 324, 1092, 0, 26698),
-        Vmstat::Cpu.new(30, 326, 838, 0, 27581)
-      ])
+                          Vmstat::Cpu.new(0, 311, 966, 0, 26_788),
+                          Vmstat::Cpu.new(1, 351, 862, 0, 27_263),
+                          Vmstat::Cpu.new(2, 324, 1092, 0, 26_698),
+                          Vmstat::Cpu.new(30, 326, 838, 0, 27_581)
+                        ])
     end
   end
 
-  context "#memory" do
+  context '#memory' do
     subject { procfs.memory }
-    
+
     it { is_expected.to be_a(Vmstat::Memory) }
     if `getconf PAGESIZE`.chomp.to_i == 4096
       it do
-        is_expected.to eq(Vmstat::Memory.new(4096, 4906, 6508, 8405, 107017, 64599, 1104))
+        is_expected.to eq(Vmstat::Memory.new(4096, 4906, 6508, 8405, 107_017, 64_599, 1104))
       end
 
-      it "should have the right total" do
+      it 'should have the right total' do
         expect(subject.wired_bytes + subject.active_bytes +
-         subject.inactive_bytes + subject.free_bytes).to eq(507344 * 1024)
+         subject.inactive_bytes + subject.free_bytes).to eq(507_344 * 1024)
       end
     end
   end
 
-  context "#boot_time" do
+  context '#boot_time' do
     subject { procfs.boot_time }
 
     it { is_expected.to be_a(Time) }
     it { Timecop.freeze(Time.now) { is_expected.to eq(Time.now - 355.63) } }
   end
 
-  context "#network_interfaces" do
+  context '#network_interfaces' do
     subject { procfs.network_interfaces }
 
     it { is_expected.to be_a(Array) }
     it do
       is_expected.to eq([
-        Vmstat::NetworkInterface.new(:lo, 3224, 0, 0, 3224, 0,
-                                     Vmstat::NetworkInterface::LOOPBACK_TYPE),
-        Vmstat::NetworkInterface.new(:eth1, 0, 1, 2, 0, 3,
-                                     Vmstat::NetworkInterface::ETHERNET_TYPE),
-        Vmstat::NetworkInterface.new(:eth0, 33660, 0, 0, 36584, 0,
-                                     Vmstat::NetworkInterface::ETHERNET_TYPE)
-      ])
+                          Vmstat::NetworkInterface.new(:lo, 3224, 0, 0, 3224, 0,
+                                                       Vmstat::NetworkInterface::LOOPBACK_TYPE),
+                          Vmstat::NetworkInterface.new(:eth1, 0, 1, 2, 0, 3,
+                                                       Vmstat::NetworkInterface::ETHERNET_TYPE),
+                          Vmstat::NetworkInterface.new(:eth0, 33_660, 0, 0, 36_584, 0,
+                                                       Vmstat::NetworkInterface::ETHERNET_TYPE)
+                        ])
     end
   end
 
-  context "#task" do
+  context '#task' do
     subject { procfs.task }
 
     it { is_expected.to be_a(Vmstat::Task) }
