a24c4c500c2cdb02fe23363d21cc1123b1455e0d
Daniel Berger <djberg96@gmail.com>
2020-05-20 04:46:56 -0400

    Applied rubocop -a to linux_memory.rb.

--- lib/vmstat/linux_memory.rb
+++ lib/vmstat/linux_memory.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # @attr [Fixnum] available
 #   The estimated available memory (linux)
 #   See: https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=34e431b0ae398fc54ea69ff85ec700722c9da773
@@ -10,4 +12,3 @@ class Vmstat::LinuxMemory < Vmstat::Memory
     available * pagesize
   end
 end
-
