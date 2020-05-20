944806522401a26ec94c91e2763051b953e33c23
Daniel Berger <djberg96@gmail.com>
2020-05-20 05:21:30 -0400

    Applied rubocop -a to task_spec.rb.

--- spec/vmstat/task_spec.rb
+++ spec/vmstat/task_spec.rb
@@ -1,42 +1,45 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe Vmstat::Task do
-  context "Vmstat#task" do
-    let(:task) { Vmstat.task }
-    subject { task }
+if RUBY_PLATFORM =~ /darwin|linux/
+  describe Vmstat::Task do
+    context 'Vmstat#task' do
+      let(:task) { Vmstat.task }
+      subject { task }
 
-    it "should be a vmstat task object" do
-      is_expected.to be_a(described_class)
-    end
-
-    context "methods" do
-      it { is_expected.to respond_to(:virtual_size) }
-      it { is_expected.to respond_to(:resident_size) }
-      it { is_expected.to respond_to(:user_time_ms) }
-      it { is_expected.to respond_to(:system_time_ms) }
-    end
-    
-    context "content" do
-      describe '#virtual_size' do
-        subject { super().virtual_size }
-        it { is_expected.to be_a_kind_of(Numeric) }
+      it 'should be a vmstat task object' do
+        is_expected.to be_a(described_class)
       end
 
-      describe '#resident_size' do
-        subject { super().resident_size }
-        it { is_expected.to be_a_kind_of(Numeric) }
+      context 'methods' do
+        it { is_expected.to respond_to(:virtual_size) }
+        it { is_expected.to respond_to(:resident_size) }
+        it { is_expected.to respond_to(:user_time_ms) }
+        it { is_expected.to respond_to(:system_time_ms) }
       end
 
-      describe '#user_time_ms' do
-        subject { super().user_time_ms }
-        it { is_expected.to be_a_kind_of(Numeric) }
-      end
+      context 'content' do
+        describe '#virtual_size' do
+          subject { super().virtual_size }
+          it { is_expected.to be_a_kind_of(Numeric) }
+        end
 
-      describe '#system_time_ms' do
-        subject { super().system_time_ms }
-        it { is_expected.to be_a_kind_of(Numeric) }
+        describe '#resident_size' do
+          subject { super().resident_size }
+          it { is_expected.to be_a_kind_of(Numeric) }
+        end
+
+        describe '#user_time_ms' do
+          subject { super().user_time_ms }
+          it { is_expected.to be_a_kind_of(Numeric) }
+        end
+
+        describe '#system_time_ms' do
+          subject { super().system_time_ms }
+          it { is_expected.to be_a_kind_of(Numeric) }
+        end
       end
     end
   end
-end if RUBY_PLATFORM =~ /darwin|linux/
-
+end
