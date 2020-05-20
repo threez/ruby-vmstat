2ea5367db6bdc1fe9425d30224454ce4952791e1
Daniel Berger <djberg96@gmail.com>
2020-05-20 05:19:46 -0400

    Applied rubocop -a to load_average_spec.rb.

--- spec/vmstat/load_average_spec.rb
+++ spec/vmstat/load_average_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Vmstat::LoadAverage do
-  context "Vmstat#load_average" do
+  context 'Vmstat#load_average' do
     subject { Vmstat.load_average }
 
-    it "should be an vmstat load average object" do
+    it 'should be an vmstat load average object' do
       is_expected.to be_a(described_class)
     end
 
-    context "methods" do
+    context 'methods' do
       it { is_expected.to respond_to(:one_minute) }
       it { is_expected.to respond_to(:five_minutes) }
       it { is_expected.to respond_to(:fifteen_minutes) }
     end
 
-    context "content" do
+    context 'content' do
       describe '#one_minute' do
         subject { super().one_minute }
         it { is_expected.to be_a(Float) }
