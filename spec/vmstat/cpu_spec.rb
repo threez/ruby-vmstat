3c8fd0ac7fff0c80dc00b4d8167a45877da7ec03
Daniel Berger <djberg96@gmail.com>
2020-05-20 05:19:09 -0400

    Applied rubocop -a to cpu_spec.rb.

--- spec/vmstat/cpu_spec.rb
+++ spec/vmstat/cpu_spec.rb
@@ -1,29 +1,31 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Vmstat::Cpu do
-  context "Vmstat#cpu" do
+  context 'Vmstat#cpu' do
     let(:cpu) { Vmstat.cpu }
 
-    it "should return an array of ethernet information" do
+    it 'should return an array of ethernet information' do
       expect(cpu).to be_a(Array)
     end
 
-    context "first cpu" do
+    context 'first cpu' do
       let(:first_cpu) { cpu.first }
       subject { first_cpu }
 
-      it "should return a vmstat cpu object" do
+      it 'should return a vmstat cpu object' do
         is_expected.to be_a(described_class)
       end
 
-      context "methods" do
+      context 'methods' do
         it { is_expected.to respond_to(:user) }
         it { is_expected.to respond_to(:system) }
         it { is_expected.to respond_to(:nice) }
         it { is_expected.to respond_to(:idle) }
       end
 
-      context "content" do
+      context 'content' do
         describe '#user' do
           subject { super().user }
           it { is_expected.to be_a_kind_of(Numeric) }
